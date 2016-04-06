3e3dbad4abf73800d98f1b0d7ca21598d6491939
tab <tab@tab-X555LJ>
2016-04-06 23:46:27 +0200

    PriorityMessageCreateEvent modified

--- src/input/PriorityMessageCreateEvent.java
+++ src/input/PriorityMessageCreateEvent.java
@@ -11,6 +11,8 @@ public class PriorityMessageCreateEvent extends MessageCreateEvent
 {
 	/** Priority of the message (0..2) */
 	protected int priority;
+	protected int psize;
+	protected int presponseSize;
 	
 	/**
 	 * Creates a message creation event with a optional response request
@@ -21,10 +23,13 @@ public class PriorityMessageCreateEvent extends MessageCreateEvent
 	 * @param responseSize Size of the requested response message or 0 if
 	 * no response is requested
 	 * @param time Time, when the message is created
+	 * @param priority The priority of the message
 	 */
 	public PriorityMessageCreateEvent(int from, int to, String id, int size, int responseSize, double time, int priority)
 	{
 		super(from, to, id, size, responseSize, time);
+		this.psize = size;
+		this.presponseSize = responseSize;
 		this.priority = priority;
 	}
 
@@ -37,8 +42,8 @@ public class PriorityMessageCreateEvent extends MessageCreateEvent
 		DTNHost to = world.getNodeByAddress(this.toAddr);
 		DTNHost from = world.getNodeByAddress(this.fromAddr);
 		
-		Message m = new PriorityMessage(from, to, this.id, this.size, this.priority);
-		m.setResponseSize(this.responseSize);
+		Message m = new PriorityMessage(from, to, this.id, this.psize, this.priority);
+		m.setResponseSize(this.presponseSize);
 		from.createNewMessage(m);
 	}
 
