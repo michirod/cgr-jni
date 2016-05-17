c515a1bbb8c7d3c56ec43d7b860fa548ddf66b51
federico <federico@ubuntu>
2016-05-17 15:01:16 +0200

    Extension with priority done. Still some issues, check later

--- src/input/ExtendedEventQueueHandler.java
+++ src/input/ExtendedEventQueueHandler.java
@@ -7,27 +7,10 @@ import core.Settings;
 
 public class ExtendedEventQueueHandler extends EventQueueHandler
 {
-	/** Event queue settings main namespace ({@value})*/
-	public static final String SETTINGS_NAMESPACE = "Events";
-	/** number of event queues -setting id ({@value})*/
-	public static final String NROF_SETTING = "nrof";
-
-	/** name of the events class (for class based events) -setting id
-	 * ({@value}) */
-	public static final String CLASS_SETTING = "class";
-	/** name of the package where event generator classes are looked from */
-	public static final String CLASS_PACKAGE = "input";
-
-	/** number of events to preload from file -setting id ({@value})*/
-	public static final String PRELOAD_SETTING = "nrofPreload";
-	/** path of external events file -setting id ({@value})*/
-	public static final String PATH_SETTING = "filePath";
-
-	private List<EventQueue> queues;
-	
 	public ExtendedEventQueueHandler()
 	{
 		super();
+		
 		Settings settings = new Settings(SETTINGS_NAMESPACE);
 		int nrof = settings.getInt(NROF_SETTING);
 	
